2ded67c74768237bd90c9ebce1fe61cd9cffa039
Jimi Patel - Github <pateljimi2020@gmail.com>
2023-08-15 22:23:27 +0530

    sevdevv

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const cors = require("cors");
 
 connectDatabase();
 const app = express();
+app.use(cookieParser());
 app.use(
   cors({
     origin: "https://mern-todo-app-server-blond.vercel.app",
@@ -18,7 +19,6 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running");
