b3109ed04a7ad8a9f90f6b4b9c08cc8d36bc51ff
Jimi Patel - Github <pateljimi2020@gmail.com>
2023-08-15 19:18:08 +0530

    error fixes

--- server/index.js
+++ server/index.js
@@ -20,6 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 app.use("/api/v1", todoRouter);
 app.use("/api/v1", userRouter);
 
